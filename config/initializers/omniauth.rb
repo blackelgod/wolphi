7edd583725bb5624e078c55d89e8132bdc982c39
Kenzo Douchet <kenzodouchet33@gmail.com>
2023-04-05 23:04:45 +0200

    test

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :discord, ENV["CLIENT_ID"], ENV["CLIENT_SECRET"], scope: 'email identify'
+  provider :discord, "1092635864502370364", "HCi1W99gWvHf1qsYMdatTvcM1YWnkW94", scope: 'email identify'
   OmniAuth.config.on_failure = Proc.new { |env|
     OmniAuth::FailureEndpoint.new(env).redirect_to_failure
   }
